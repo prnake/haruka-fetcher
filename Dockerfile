c4df285b339145eed1dbd97c6e50332c86272dbd
papersnake <prnake@gmail.com>
2024-01-15 08:46:59 +0000

    feat: add retry

--- Dockerfile
+++ Dockerfile
@@ -15,4 +15,4 @@ COPY . .
 
 EXPOSE 3000
 
-CMD uvicorn main:app --host 0.0.0.0 --port 3000
+CMD uvicorn main:app --host 0.0.0.0 --port 3000 --workers 4
--- main.py
+++ main.py
@@ -3,15 +3,21 @@ from fastapi import FastAPI, Depends, HTTPException, status
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 import asyncio
 import os
+import random
 import shutil
 import requests
 import requests_go
 from utils import get_tls, get_headers, get_ua
 from haruka_parser.extract import extract_text
 from haruka_parser.extract import DEFAULT_CONFIG as configuration
+import charset_mnbvc.api
+
+import socket
+socket.setdefaulttimeout(20)
 
 import fitz
 import logging
+import time
 import timeout_decorator
 
 logger = logging.getLogger("uvicorn.error")
@@ -21,12 +27,30 @@ from apryse_sdk import *
 
 configuration["readability"] = True
 
-PROXY = os.environ.get("PROXY", "")
+PROXY = os.environ.get("PROXY", "").split(",")
 BROSWER_API = os.environ.get("BROSWER_API", "")
-BROWSER_PROXY_SERVER = os.environ.get("BROWSER_PROXY_SERVER", "")
-BROWSER_PROXY_USERNAME = os.environ.get("BROWSER_PROXY_USERNAME", "")
-BROWSER_PROXY_PASSWORD = os.environ.get("BROWSER_PROXY_PASSWORD", "")
 BEARER_TOKEN = os.environ.get("BEARER_TOKEN", "")
+
+def get_proxy_config(proxy):
+    protocol, proxy = proxy.split('://', 1)
+    parts = proxy.split('@')
+    if len(parts) == 2:
+        auth = parts[0].split(':')
+        server = parts[1]
+        if len(auth) == 2:
+            username = auth[0]
+            password = auth[1]
+        else:
+            username = ""
+            password = ""
+    else:
+        server = proxy
+        username = ""
+        password = ""
+    return f"{protocol}://{server}", username, password
+
+for proxy in PROXY:
+    get_proxy_config(proxy)
 
 app = FastAPI()
 
@@ -35,13 +59,14 @@ security = HTTPBearer()
 def pdf_to_html_fitz(pdf_path):
     doc = fitz.open(pdf_path)
     html_content = ""
-    for page_num in range(len(doc)):
+    page_cnt = len(doc)
+    for page_num in range(page_cnt):
         page = doc.load_page(page_num)
         text_lines = page.get_text("text").split('\n')
         for line in text_lines:
             html_content += f"<p>{line}</p>"
     doc.close()
-    return html_content
+    return html_content, page_cnt
 
 def pdf_to_html(pdf_content, timeout):
     pdf_tmp_id = str(uuid4())
@@ -55,42 +80,43 @@ def pdf_to_html(pdf_content, timeout):
     fallback_html = ""
 
     try:
-        fallback_html = pdf_to_html_fitz(pdf_tmp_path)
+        fallback_html, page_cnt = pdf_to_html_fitz(pdf_tmp_path)
     except Exception as e:
         logger.error(f"error: fitz {str(e)}")
         pass
 
-    htmlOutputOptions = HTMLOutputOptions()
+    if page_cnt <= 5:
+        htmlOutputOptions = HTMLOutputOptions()
 
-    htmlOutputOptions.SetConnectHyphens(True)
-    htmlOutputOptions.SetEmbedImages(False)
-    htmlOutputOptions.SetFileConversionTimeoutSeconds(timeout)
+        htmlOutputOptions.SetConnectHyphens(True)
+        htmlOutputOptions.SetEmbedImages(False)
+        htmlOutputOptions.SetFileConversionTimeoutSeconds(timeout)
 
-    @timeout_decorator.timeout(timeout)
-    def convert_to_html():
-        html = ""
-        try:
-            htmlOutputOptions.SetContentReflowSetting(HTMLOutputOptions.e_reflow_full)
-            Convert.ToHtml(pdf_tmp_path, html_tmp_path, htmlOutputOptions)
-            html = open(html_tmp_path, encoding="utf-8").read()
-        except Exception as e:
-            logger.error(f"error: e_reflow_full {str(e)}")
-            pass
-
-        if not html:
+        # @timeout_decorator.timeout(timeout)
+        def convert_to_html():
+            html = ""
             try:
-                htmlOutputOptions.SetContentReflowSetting(HTMLOutputOptions.e_reflow_paragraphs)
+                htmlOutputOptions.SetContentReflowSetting(HTMLOutputOptions.e_reflow_full)
                 Convert.ToHtml(pdf_tmp_path, html_tmp_path, htmlOutputOptions)
                 html = open(html_tmp_path, encoding="utf-8").read()
             except Exception as e:
-                logger.error(f"error: e_reflow_paragraphs {str(e)}")
+                logger.error(f"error: e_reflow_full {str(e)}")
                 pass
-        return html
-    
-    try:
-        html = convert_to_html()
-    except Exception as e:
-        logger.error(f"error: apryse {str(e)}")
+
+            if not html:
+                try:
+                    htmlOutputOptions.SetContentReflowSetting(HTMLOutputOptions.e_reflow_paragraphs)
+                    Convert.ToHtml(pdf_tmp_path, html_tmp_path, htmlOutputOptions)
+                    html = open(html_tmp_path, encoding="utf-8").read()
+                except Exception as e:
+                    logger.error(f"error: e_reflow_paragraphs {str(e)}")
+                    pass
+            return html
+        
+        try:
+            html = convert_to_html()
+        except Exception as e:
+            logger.error(f"error: apryse {str(e)}")
     
     if not html:
         html = fallback_html
@@ -101,18 +127,65 @@ def pdf_to_html(pdf_content, timeout):
         pass
     return html
 
-async def fetch_pdf_url(url: str, timeout, source="", params=None, proxy=None):
-    try:
-        headers = get_headers()
-        tls = get_tls()
-        if proxy and proxy != "playwright_proxy":
-            response = await requests_go.async_get(url, params=params, headers=headers, tls_config=tls, proxies={"http": proxy, "https": proxy}, timeout=timeout)
-        else:
-            response = await requests_go.async_get(url, params=params, headers=headers, tls_config=tls, timeout=timeout)
+def fetch_url(url: str, timeout, source="", params=None, proxy=None):
+    # @timeout_decorator.timeout(timeout)
+    def _fetch_url_timeout():
+        try:
+            headers = get_headers()
+            tls = get_tls()
+            if proxy and proxy != "playwright_proxy":
+                response = requests_go.get(url, params=params, headers=headers, proxies={"http": proxy, "https": proxy}, tls_config=tls, timeout=timeout)
+            else:
+                response = requests.get(url, params=params, headers=headers, timeout=timeout)
 
-        if response.status_code == 200:
-            html = pdf_to_html(response.content, timeout)
-            return {"html": html, "proxy": proxy is not None, "source": source, "status": 200}
+            if response.status_code == 200:
+                if source == "playwright":
+                    res = response.json()
+                    res["proxy"] = proxy is not None
+                    res["source"] = "playwright"
+                    res["status"] = 200
+                    return res
+                
+                content = response.content
+                content_type = response.headers.get('content-type', '')
+                if 'pdf' in content_type or content.startswith(b'%PDF-'):
+                    html = pdf_to_html(content, timeout)
+                    return {"html": html, "proxy": proxy is not None, "source": "pdf", "status": 200}
+                elif b'\x00' in content:
+                    return None
+                else:
+                    encoding = charset_mnbvc.api.from_data(data=content, mode=2)
+                    html_result = ""
+                    try:
+                        html_result = content.decode(encoding)
+                    except Exception as e:
+                        logger.error(f"Error parse url {url} {e}")
+                        pass
+                    try:
+                        html_result = content.decode('utf-8', errors='ignore')
+                    except Exception as e:
+                        logger.error(f"Error parse url {url} {e}")
+                        pass
+                    if html_result:
+                        return {"html": html_result, "proxy": proxy is not None, "encoding": encoding, "source": source, "status": 200}
+                    else:
+                        return None
+        except Exception as e:
+            if proxy:
+                logger.error(f"Error fetching {url} with proxy: {e}")
+            else:
+                logger.error(f"Error fetching {url}: {e}")
+            return None
+    try:
+        start_time = time.time()
+        retry_time = 1 if source == "playwright" else 5
+        for _ in range(retry_time):
+            res = _fetch_url_timeout()
+            if res:
+                return res
+            if time.time() - start_time > timeout:
+                return None
+        return None
     except Exception as e:
         if proxy:
             logger.error(f"Error fetching {url} with proxy: {e}")
@@ -120,29 +193,7 @@ async def fetch_pdf_url(url: str, timeout, source="", params=None, proxy=None):
             logger.error(f"Error fetching {url}: {e}")
         return None
 
-async def fetch_url(url: str, timeout, source="", params=None, proxy=None):
-    try:
-        headers = get_headers()
-        tls = get_tls()
-        if proxy and proxy != "playwright_proxy":
-            response = await requests_go.async_get(url, params=params, headers=headers, tls_config=tls, proxies={"http": proxy, "https": proxy}, timeout=timeout)
-        else:
-            response = await requests_go.async_get(url, params=params, headers=headers, tls_config=tls, timeout=timeout)
-
-        if response.status_code == 200:
-            if source == "playwright":
-                res = response.json()
-                res["proxy"] = proxy is not None
-                res["source"] = "playwright"
-                res["status"] = 200
-                return res
-            else:
-                return {"html": response.text, "proxy": proxy is not None, "source": source, "status": 200}
-    except Exception as e:
-        logger.error(f"Error fetching {url}: {e}")
-        return None
-
-async def get_content_type(url: str):
+def get_content_type(url: str):
     try:
         with requests.get(url, stream=True, timeout=3) as response:
             response.raise_for_status()
@@ -153,8 +204,38 @@ async def get_content_type(url: str):
         pass
     return 'unknown'
 
+async def wait_for_thread(func, args=(), timeout=10):
+    """
+    Runs 'func' in a separate thread with 'args' and waits for at most 'timeout' seconds.
+    If 'func' does not complete within 'timeout', asyncio.TimeoutError is raised.
+    """
+    try:
+        return await asyncio.wait_for(asyncio.to_thread(func, *args), timeout)
+    except asyncio.TimeoutError:
+        logger.info(f"The url {args[0]} took too long and was cancelled.")
+        return None
+    except Exception as e:
+        # Handle other exceptions that the function might raise
+        logger.error(f"Error thread url {args[0]}: {e}")
+        return None
+
 @app.get("/browser")
-async def browser(credentials: Annotated[str, Depends(security)], url: str, timeout: int = 10, pdf_timeout = None):
+async def browser(credentials: Annotated[str, Depends(security)], url: str, timeout: int = 9):
+
+    start_time = time.time()
+
+    def remaining_timeout(real=False):
+        if real:
+            return timeout + start_time - time.time()
+        return int(max(timeout + start_time - time.time(), 2))
+    def spending_time():
+        return time.time() - start_time
+    
+    # def remaining_pdf_timeout(real=False):
+    #     if real:
+    #         return remaining_pdf_timeout + start_time - time.time()
+    #     return int(max(timeout + start_time - time.time(), 2))
+
     if credentials:
         token = credentials.credentials
         if token == BEARER_TOKEN:
@@ -165,47 +246,61 @@ async def browser(credentials: Annotated[str, Depends(security)], url: str, time
                 detail="Invalid authentication credentials",
             )
     
-    if not pdf_timeout:
-        pdf_timeout = timeout
-    
-    content_type = await get_content_type(url)
+    # content_type = await get_content_type(url)
 
     tasks = []
 
-    if "pdf" in content_type:
-        tasks.append(fetch_pdf_url(url, timeout, source="pdf"))
-        if PROXY:
-            tasks.append(fetch_pdf_url(url, timeout, source="pdf", proxy=PROXY))
+    # if "pdf" in content_type:
+    #     tasks.append(fetch_pdf_url(url, remaining_timeout(), source="pdf"))
+    #     if PROXY:
+    #         tasks.append(fetch_pdf_url(url, remaining_timeout(), source="pdf", proxy=PROXY))
 
-    else:
-        tasks.append(fetch_url(url, timeout, source="get"))
+    # else:
 
-        if PROXY:
-            tasks.append(fetch_url(url, timeout, source="proxy", proxy=PROXY))
+    if PROXY:
+        proxy = random.choice(PROXY)
+        proxy_server, proxy_username, proxy_password = get_proxy_config(proxy)
 
-        if BROSWER_API:
-            browser_params = {"url": url, "timeout": int(max(timeout-2,2)*1000), "user-agent": get_ua()}
-            tasks.append(fetch_url(f"{BROSWER_API}/api/article", timeout, source="playwright", params=browser_params))
-            if BROWSER_PROXY_SERVER and BROWSER_PROXY_USERNAME and BROWSER_PROXY_PASSWORD:
-                browser_params.update({"proxy-server": BROWSER_PROXY_SERVER, "proxy-username": BROWSER_PROXY_USERNAME, "proxy-password": BROWSER_PROXY_PASSWORD})
-                tasks.append(fetch_url(f"{BROSWER_API}/api/article", timeout, source="playwright", params=browser_params, proxy="playwright_proxy"))
+    tasks.append(wait_for_thread(fetch_url, (url, timeout, "get"), timeout))
 
+    if proxy:
+        tasks.append(wait_for_thread(fetch_url, (url, timeout, "proxy", None, proxy), timeout))
+
+    if BROSWER_API and proxy_server:
+        browser_params = {"url": url, "timeout": int(timeout*1000), "user-agent": get_ua()}
+        browser_params.update({"proxy-server": proxy_server, "proxy-username": proxy_username, "proxy-password": proxy_password})
+        tasks.append(wait_for_thread(fetch_url, (f"{BROSWER_API}/api/article", timeout, "playwright", browser_params, "playwright_proxy"), timeout))
+        
+        # tasks.append(fetch_url(f"{BROSWER_API}/api/article", timeout, source="playwright", params=browser_params))
+    def cancel_all_task():
+        pass
+        # for task in tasks:
+        #     if task and not task.done():
+        #         task.cancel()
     best_result = {}
-    for future in asyncio.as_completed(tasks):
-        result = await future
-        if result:
-            if result["source"] == "pdf":
-                return result
-            try:
-                result["content"] = extract_text(result["html"], configuration)[0]
-            except:
-                result["content"] = ""
-            if len(result["content"]) > 100:
-                return result
-            if len(best_result.get("html", "")) < len(result.get("html", "")):
-                best_result = result
+    try:
+        for future in asyncio.as_completed(tasks):
+            result = await future
+            if result:
+                if result["source"] == "pdf":
+                    logger.info(f"{spending_time()} {url}")
+                    cancel_all_task()
+                    return result
+                try:
+                    result["content"] = extract_text(result["html"], configuration)[0]
+                except:
+                    result["content"] = ""
+                if len(result["content"]) > 100:
+                    logger.info(f"{spending_time()} {url}")
+                    cancel_all_task()
+                    return result
+                if len(best_result.get("html", "")) < len(result.get("html", "")):
+                    best_result = result
+    except:
+        cancel_all_task()
     
     if best_result:
+        logger.info(f"{spending_time()} {url}")
         return best_result
 
     raise HTTPException(status_code=500, detail="No content fetched from any source")
